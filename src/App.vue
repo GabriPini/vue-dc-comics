9e87f33df5cb88912d5c4e9ccf1b34999ed7a1c2
GabriPini <lele19977@gmail.com>
2022-05-03 14:05:05 +0200

    Header part done , and add a commeted structure of page

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,26 @@
 <template>
   <div id="app">
-
+    <SiteHeader />
+<!--     <SiteMain />
+    <SiteFooter /> -->
   </div>
 </template>
 
 <script>
-
-
+import SiteHeader from './components/headerComponent.vue'
+/* import SiteMain from './components/mainComponent.vue'
+import SiteFooter from './components/footerComponent.vue' */
 export default {
   name: 'App',
   components: {
-    
+    SiteHeader,
+/*     SiteMain,
+    SiteFooter */
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './style/general.scss';
+@import './style/variables.scss';
 </style>
---
+++ src/components/headerComponent.vue
@@ -0,0 +1,130 @@
+<template>
+    <header>
+        <section>
+            <nav id="main-navbar">
+                <div class="main-logo">
+                    <a href="#">
+                        <img src="@/assets/img/dc-logo.png" alt="main-dc-logo">
+                    </a>
+                </div>
+
+                <ul>
+
+                    <li v-for="(element, index) in navElement " :key="index">
+                        <a :class="{ active : element.current }" href="#"> {{ element.name }} </a>
+                    </li>
+                    
+                </ul>
+
+
+            </nav>
+        </section>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'SiteHeader',
+    data() {
+        return {
+            navElement : [
+                {
+                    name: 'characters',
+                    url : '#',
+                    current : false
+                },
+                {
+                    name: 'comics',
+                    url : '#',
+                    current : true
+                },
+                {
+                    name: 'movies',
+                    url : '#',
+                    current : false
+                },
+                {
+                    name: 'tv',
+                    url : '#',
+                    current : false
+                },
+                {
+                    name: 'games',
+                    url : '#',
+                    current : false
+                },
+                {
+                    name: 'collectibles',
+                    url : '#',
+                    current : false
+                },
+                {
+                    name: 'videos',
+                    url : '#',
+                    current : false
+                },
+                {
+                    name: 'fans',
+                    url : '#',
+                    current : false
+                },
+                {
+                    name: 'news',
+                    url : '#',
+                    current : false
+                },
+                {
+                    name: 'shop',
+                    url : '#',
+                    current : false
+                },
+            ]
+        }
+        
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/style/variables.scss';
+ul{
+    list-style: none;
+    li>a{
+        text-decoration: none;
+        color: $linkColor;
+    }
+}
+#main-navbar{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 100px;
+    .main-logo{
+        padding: 7.5px;
+        img{
+            width: 75px;
+        }
+        
+    }
+    ul{
+        display: flex;
+        justify-content: space-around;
+        flex-basis: calc(100% / 2);
+        height: 100%;
+        a{
+            display: inline-block;
+            height: 100px;
+            line-height: 100px;
+            text-transform: uppercase;
+            font-size: 0.85rem;
+            font-weight: 600;
+            color: black;
+            line-height: 100px;
+        }
+        a.active{
+            border-bottom: 4px solid $mainBlueColor;
+            color: $mainBlueColor;
+        }
+    }
+}
+</style>
